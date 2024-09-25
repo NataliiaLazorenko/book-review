da72be189b6e9dad1281a180c94dac14682ed378
Nataliia Lazorenko <nataly.lazorenko@speedandfunction.com>
2024-09-26 00:51:45 +0300

    s4-43 Querying & associating related models

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -9,6 +9,8 @@ class Review extends Model
 {
     use HasFactory;
 
+    protected $fillable = ['review', 'rating'];
+
     // The 'belongsTo' method defines the inverse side of the one-to-many relationship, specifying that each review belongs to one book
     public function book()
     {
