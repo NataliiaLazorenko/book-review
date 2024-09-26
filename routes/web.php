6a51460da64de3830fec91fb0f6271cddb3ee873
Nataliia Lazorenko <nataly.lazorenko@speedandfunction.com>
2024-09-27 00:19:14 +0300

    s5-49 Controllers & resource controllers

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
+use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Register the route for a resource controller
+Route::resource('books', BookController::class);
