22e4f6fc4ebd0e8abb6a3631309f555fa0f05aea
Nataliia Lazorenko <nataly.lazorenko@speedandfunction.com>
2024-09-28 16:20:27 +0300

    s5-52 Popular or highest rated - the view

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -5,16 +5,59 @@
 
 {{-- This form allows searching by 'title'. 
 'GET' is the default method, but we're including it explicitly for clarity. 
-The current route is set as 'action' to send the form data back to the same page --}}
+The current route is set as 'action' to send the form data back to the same page
+
+1. Keeping the active tab (filter) selected during searches.
+We use the form since it controls the query parameters sent and hidden input.
+In GET form, all submitted values appear as query variables in the URL, unlike POST requests, where data is sent in the request body and isn't visible in the URL.
+Hidden input is not visible to the user but allows to send any value, such as the current filter, along with the form data. --}}
 <form method="GET" action="{{ route('books.index') }}" class="mb-4 flex items-center space-x-2">
     {{-- The 'name' attribute is required to identify the input and will be included with the form submission.
     The 'request' function is used to show the previous input value. If the form was submitted before, 
     this input will be pre-filled with the last used filter value --}}
     <input type="text" name="title" placeholder="Search by title" value="{{ request('title') }}" class="input h-10" />
+    <input type="hidden" name="filter" value={{ request('filter') }} />
     <button type="submit" class="btn h-10">Search</button>
     <!-- The simplest way to clear the form is to link to the page without query parameters -->
     <a href="{{ route('books.index') }}" class="btn h-10">Clear</a>
 </form>
+
+<div class="filter-container mb-4 flex">
+    {{-- Using the 'php' directive in Blade --}}
+    @php
+    /*
+    * This array contains keys that represent what to pass to the query parameters for the request, allowing specific queries to be performed.
+    * We could define this array in the controller and pass it along, but since it's not directly tied to the logic,
+    * it makes sense to define it here in the template.
+    */
+    $filters = [
+    '' => 'Latest',
+    'popular_last_month' => 'Popular Last Month',
+    'popular_last_6months' => 'Popular Last 6 Months',
+    'highest_rated_last_month' => 'Highest Rated Last Month',
+    'highest_rated_last_6months' => 'Highest Rated Last 6 Months',
+    ]
+    @endphp
+
+    {{-- Using key-value pairs: the key identifies the filter, and the label is displayed to the user --}}
+    @foreach ($filters as $key => $label)
+    {{-- A query parameter (in our case, its name is 'filter') can be passed as an array in the second parameter of the 'route' function.
+    The 'request' helper gives us access to the 'filter' query parameter
+    
+    2. Selecting the 'Latest' tab by default when no filter is specified or if it is empty.
+    We update the condition for applying the class: if the query parameter 'filter' is null or
+    if the current tab is the first element of the filters (the 'Latest' tab), we apply the 'filter-item-active' class.
+    
+    3. Retaining all other request parameters when navigating to any of the tab links.
+    To keep all other request parameters while switching between tab links, we extend the array of query parameters.
+    This is achieved using the request()->query() method, which returns an array of all current query parameters.
+    Since this method always returns an array and we're already inside an array context, we utilize the spread operator --}}
+    <a href="{{ route('books.index', [...request()->query(), 'filter' => $key]) }}"
+        class="{{ request('filter') === $key || (request('filter') === null && $key === '') ? 'filter-item-active' : 'filter-item' }}">
+        {{ $label }}
+    </a>
+    @endforeach
+</div>
 
 <ul>
     @forelse ( $books as $book)
