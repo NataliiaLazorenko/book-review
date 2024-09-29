be543b8cf1e04de3fe6e30322bd8c9f8b037b0cd
Nataliia Lazorenko <nataly.lazorenko@speedandfunction.com>
2024-09-29 23:49:19 +0300

    s5-58 Blade components - star rating component

--- resources/views/books/index.blade.php
+++ resources/views/books/index.blade.php
@@ -35,7 +35,7 @@ Hidden input is not visible to the user but allows to send any value, such as th
     'popular_last_month' => 'Popular Last Month',
     'popular_last_6months' => 'Popular Last 6 Months',
     'highest_rated_last_month' => 'Highest Rated Last Month',
-    'highest_rated_last_6months' => 'Highest Rated Last 6 Months',
+    'highest_rated_last_6months' => 'Highest Rated Last 6 Month',
     ]
     @endphp
 
@@ -72,7 +72,8 @@ Hidden input is not visible to the user but allows to send any value, such as th
                 <div>
                     <div class="book-rating">
                         {{-- number_format is a PHP function. The '1' specifies one decimal place after the comma or dot (based on locale settings) --}}
-                        {{ number_format($book->reviews_avg_rating, 1) }}
+                        {{-- {{ number_format($book->reviews_avg_rating, 1) }} --}}
+                        <x-star-rating :rating="$book->reviews_avg_rating" />
                     </div>
                     <div class="book-review-count">
                         {{-- 'Str::plural' method allows to display the singular or plural form of the word based on the count. It excepts a noun and a count
