f6b8b840638b16a21e4882820bfc94bfa5b612ee
Nataliia Lazorenko <nataly.lazorenko@speedandfunction.com>
2024-09-30 01:46:20 +0300

    s5-59 Scoped resource controller - adding a review

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,21 @@
 <?php
 
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\ReviewController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return redirect()->route('books.index');
 });
 
-// Register the route for a resource controller
-Route::resource('books', BookController::class);
+/*
+ * Register the route for a resource controller
+ * We don't need routes for all actions. We can use the 'only' method to specify which routes to enable. All others will be disabled.
+ */
+Route::resource('books', BookController::class)
+    ->only(['index', 'show']);
+
+// We scope the routes so that reviews are associated with a specific book
+Route::resource('books.reviews', ReviewController::class)
+    ->scoped(['review' => 'book'])
+    ->only(['create', 'store']);
